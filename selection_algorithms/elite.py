7482b1f18f17c8c3a4dc606eb15eb04e86fe7542
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-08-30 15:50:21 -0300

    fit: small bug fix

--- selection_algorithms/elite.py
+++ selection_algorithms/elite.py
@@ -4,10 +4,11 @@ from classes.generation import Generation
 
 def elite_selection(amount_to_select, generation: Generation):
     generation.sort(reverse=True)
-    selection = Generation
+    selection = Generation()
+    gen_size = len(generation)
 
     for i in range(amount_to_select):
-        selection.append(generation[i % len(generation)])
+        selection.append(generation[i % gen_size])
 
     return selection
 
