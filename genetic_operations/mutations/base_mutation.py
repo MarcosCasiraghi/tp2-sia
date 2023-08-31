50e802e58965d1891ad28e6eeab7c278f2f56509
Marcos Gronda <marcosgronda@gmail.com>
2023-08-31 10:06:37 -0300

    feat: uniform and limited mutation

--- genetic_operations/mutations/base_mutation.py
+++ genetic_operations/mutations/base_mutation.py
@@ -1,15 +1,17 @@
-import random
+from genetic_operations.mutations.limited_multigen import mutate_limited_multi_gen
 from genetic_operations.mutations.single_gene import mutate_single_gene
+from genetic_operations.mutations.uniform_multigen import mutate_uniform_multi_gen
 
 mutation_map = {
-    "single_gene": mutate_single_gene
+    "single_gene": mutate_single_gene,
+    "mutate_limited_multi_gen": mutate_limited_multi_gen,
+    "mutate_uniform_multi_gen": mutate_uniform_multi_gen
 }
 
 
 def mutate_populus(populus, config):
-    pm = config["mutation_prob"]
     method = mutation_map[config["mutation_method"]]
 
     for character in populus:
-        if random.random() < pm:
-            method(character, config)
+        method(character, config)
+
---
+++ genetic_operations/mutations/uniform_multigen.py
@@ -0,0 +1,18 @@
+import random
+
+from classes.fighters import Fighter, Genes
+from genetic_operations.mutations.gene_utils import  mutate_gene
+
+
+def mutate_uniform_multi_gen(character: Fighter, config):
+
+    pm = config["mutation_prob"]
+    mutation_change = config["mutation_change"]
+
+    for i in range(len(Genes)):
+
+        # Primero vemos si le toca mutar este gen especifico
+        if random.random() >= pm:
+            continue
+
+        mutate_gene(character, i, mutation_change)
