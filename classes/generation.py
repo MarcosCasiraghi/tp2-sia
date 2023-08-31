709b3a4b760f9a2ef0ff8ad08ebd7f620322710b
Agustin Z <86991224+azakalik@users.noreply.github.com>
2023-08-31 09:38:50 -0300

    Update generation.py

--- classes/generation.py
+++ classes/generation.py
@@ -1,5 +1,3 @@
-# esta clase extiende a una lista, por lo que se pueden usar los metodos append y eso
-# para crear nuevas generaciones usamos new_empty_generation asi el numero se aumenta solo
 
 class Generation(list):
     class_number = 0
@@ -8,6 +6,3 @@ class Generation(list):
         super().__init__()
         self.generation_number = Generation.class_number
         Generation.class_number += 1
-
-    def new_empty_generation(self):
-        return self.__init__()
