7e50d9f2589f661962106e7991b4656e5e539792
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-08-31 10:15:36 -0300

    feat: boltzmann selection, roulette modularized

--- selection_algorithms/roulette.py
+++ selection_algorithms/roulette.py
@@ -1,27 +1,33 @@
 from classes.generation import Generation
 import random
+import numpy as np
 
 
 def roulette_selection(amount_to_select, generation: Generation):
-    total_fitness = 0
-    normalized_fitness = []
+    fitness_array = []
     # calculo el fitness total y agrego fitness en arreglo para despues normalizarlo
     for elem in generation:
         fitness = elem.get_performance()
-        normalized_fitness.append(fitness)
-        total_fitness += fitness
+        fitness_array.append(fitness)
 
-    #normalizo cada fitness a partir de valor total
-    for i in range(len(normalized_fitness)):
-        normalized_fitness[i] /= total_fitness
+    return roulette_random_selection(amount_to_select, fitness_array, generation)
 
-    #calculo fitness accumulativo a partir del normalizado
+
+def roulette_random_selection(amount_to_select, fitness, generation: Generation):
+    total_fitness = np.sum(fitness)
+
+    # normalizo cada fitness a partir de valor total
+    normalized_fitness = []
+    for elem in fitness:
+        normalized_fitness.append(elem/total_fitness)
+
+    # calculo fitness accumulativo a partir del normalizado
     accumulative_fitness = []
     for i in range(len(normalized_fitness)):
         if i == 0:
             accumulative_fitness.append(normalized_fitness[i])
         else:
-            accumulative_fitness.append(normalized_fitness[i] + accumulative_fitness[i-1])
+            accumulative_fitness.append(normalized_fitness[i] + accumulative_fitness[i - 1])
 
     # Calculo numero aleatorio y selecciono los fighters
     selected = []
