1bb37813fa7b95e0bea197d81e9e5f6fe26d079b
Marcos Gronda <marcosgronda@gmail.com>
2023-09-06 16:39:50 -0300

    refactor: simplified elite selection

--- selection_algorithms/elite.py
+++ selection_algorithms/elite.py
@@ -4,10 +4,4 @@ from classes.generation import Generation
 
 def elite_selection(amount_to_select, populus):
     populus.sort(reverse=True)
-    selection = []
-    gen_size = len(populus)
-
-    for i in range(amount_to_select):
-        selection.append(populus[i % gen_size])
-
-    return selection
+    return populus[0: amount_to_select]
