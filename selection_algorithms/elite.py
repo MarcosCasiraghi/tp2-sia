55babd7d684f701063d2fc5543e98ae63040f445
Marcos Gronda <marcosgronda@gmail.com>
2023-09-06 17:40:31 -0300

    Revert "refactor: simplified elite selection"
    This reverts commit 1bb37813fa7b95e0bea197d81e9e5f6fe26d079b.

--- selection_algorithms/elite.py
+++ selection_algorithms/elite.py
@@ -4,4 +4,10 @@ from classes.generation import Generation
 
 def elite_selection(amount_to_select, populus):
     populus.sort(reverse=True)
-    return populus[0: amount_to_select]
+    selection = []
+    gen_size = len(populus)
+
+    for i in range(amount_to_select):
+        selection.append(populus[i % gen_size])
+
+    return selection
