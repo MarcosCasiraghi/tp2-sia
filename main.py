d0727d368e073b60702c19e506653c8a57ce248b
Marcos Gronda <marcosgronda@gmail.com>
2023-09-06 12:14:29 -0300

    feat: metric collection

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
+import time
+
 from cutoff_criteria.base_cutoff import cutoff
 from genetic_operations.crossings.base_crossing import cross_populus
 from genetic_operations.mutations.base_mutation import mutate_populus
 from selection_algorithms.base_selection import select_populus, replace_populus
 from utils.gen_zero import gen_zero
+from utils.metrics import *
 
 
 # # la idea aca es levantar la configuracion de un JSON y cambiar la funcion create_fighter por el constructor adecuado
@@ -13,15 +16,23 @@ from utils.gen_zero import gen_zero
 def main():
     config = json.load(open("./config.json"))
 
-    # Generate gen 0
-    gen = gen_zero(config)
+    # Aca pondremos las distintas metricas medidas a lo largo del algoritmo
+    metrics = {}
+
+    # Guardamos la configuracion usada
+    collect_config(config, metrics)
 
     # Informacion sobre las generaciones anteriores (por si lo necesita el criterio de corte)
     prev_gen_info = {}
 
+    # = - = - = Comienza el algoritmo = - = - =
+    start_time = time.time()
+
+    # Generamos la generacion cero
+    gen = gen_zero(config)
+
     # Ciclo con criterio de corte
     while not cutoff(gen, config, prev_gen_info):
-
         # Hacemos la seleccion
         selected = select_populus(gen, config)
 
@@ -34,9 +45,13 @@ def main():
         # Hacemos el reemplazo (usando los algoritmos de seleccion)
         gen = replace_populus(children, gen, config)
 
-    gen.sort(reverse=True)
-    print(f"The best character is:\n {gen[0]}")
+    # = - = - = Termina el algoritmo = - = - =
+    end_time = time.time()
+
+    collect_time_metrics(start_time, end_time, metrics)
+    collect_metrics_finished(gen, metrics)
+
+    export_metrics(metrics)
 
 
 main()
-
---
+++ utils/metrics.py
@@ -0,0 +1,81 @@
+import json
+from datetime import datetime
+
+
+# = = = = = = = = = =  Side functions  = = = = = = = = = =
+
+def collect_best_in_gen(gen):
+    best = max(gen)
+
+    best_in_gen = {
+        "performance": best.performance, "strength": best.strength, "agility": best.agility,
+        "expertise": best.expertise, "resistence": best.resistence, "hp": best.hp, "height": best.height
+    }
+
+    return best_in_gen
+
+
+def collect_worst_in_gen(gen):
+    best = min(gen)
+
+    worst_in_gen = {
+        "performance": best.performance, "strength": best.strength, "agility": best.agility,
+        "expertise": best.expertise, "resistence": best.resistence, "hp": best.hp, "height": best.height
+    }
+
+    return worst_in_gen
+
+
+def collect_average_in_gen(gen):
+    # NOTAR: si vamos a estar usando esto para cada generacion, es mas eficiente usar variables directamente
+    # en vez de un diccionario (que tenes que constantemente calcular el hash)
+
+    performance_sum = 0
+    strength_sum = 0
+    agility_sum = 0
+    expertise_sum = 0
+    resistence_sum = 0
+    hp_sum = 0
+    height_sum = 0
+
+    for elem in gen:
+        performance_sum += elem.performance
+        strength_sum += elem.strength
+        agility_sum += elem.agility
+        expertise_sum += elem.expertise
+        resistence_sum += elem.resistence
+        hp_sum += elem.hp
+        height_sum += elem.height
+
+    gen_size = len(gen)
+    resp = {
+        "performance": performance_sum / gen_size, "strength": strength_sum / gen_size, "agility": agility_sum / gen_size,
+        "expertise": expertise_sum / gen_size, "resistence": resistence_sum / gen_size, "hp": hp_sum / gen_size, "height": height_sum / gen_size
+    }
+
+    return resp
+
+
+# = = = = = = = = = =  Main functions = = = = = = = = = =
+
+
+def collect_config(config, metrics):
+    metrics["config_used"] = config
+
+
+def collect_time_metrics(start, end, metrics):
+    metrics["execution_time"] = end - start
+
+
+def collect_metrics_finished(final_gen, metrics):
+    metrics["number_of_generations"] = final_gen.generation_number
+
+    metrics["best_in_final_gen"] = collect_best_in_gen(final_gen)
+    metrics["worst_in_final_gen"] = collect_worst_in_gen(final_gen)
+    metrics["average_in_final_gen"] = collect_average_in_gen(final_gen)
+
+
+def export_metrics(metrics):
+    now = datetime.now().strftime("%d-%m-%Y_%H%M%S")
+    with open(f"./results/results_{now}.json", mode="w") as file:
+        file.write(json.dumps(metrics, indent=4))
