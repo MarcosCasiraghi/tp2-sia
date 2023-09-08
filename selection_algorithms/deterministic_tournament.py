909246e9d1325a6aeeeac2fa8cc03b5bc6cc8945
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2023-09-08 09:15:04 -0300

    fix: fighter append instead of entire generation in deterministic tournament

--- selection_algorithms/deterministic_tournament.py
+++ selection_algorithms/deterministic_tournament.py
@@ -10,7 +10,7 @@ def deterministic_tournament_selection(amount_to_select, populus, M=3):
         for i in range(0, M):
             random = randint(0, generation_len-1)
             if populus[random] not in tournament_array:
-                tournament_array.append(populus)
+                tournament_array.append(populus[random])
             else:
                 i -= 1
         tournament_array.sort(reverse=True)
