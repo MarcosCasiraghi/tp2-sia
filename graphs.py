d1144591a11b14a3d46fc6a71fa633325265231d
Marcos Gronda <marcosgronda@gmail.com>
2023-09-09 09:31:45 -0300

    feat: rate of change in gene variation

--- graphs.py
+++ graphs.py
@@ -25,9 +25,42 @@ def generate_gene_variation_graph(results):
     fig = go.Figure(data=traces, layout=layout)
     fig.show()
 
+def generate_variation_rate_of_change_graph(results):
+
+    strength_variation = results["strength_variation"]
+    agility_variation = results["agility_variation"]
+    expertise_variation = results["expertise_variation"]
+    resistance_variation = results["resistance_variation"]
+    hp_variation = results["hp_variation"]
+
+    strength_rate = [0] + [strength_variation[i] - strength_variation[i - 1] for i in range(1, len(strength_variation))]
+    agility_rate = [0] + [agility_variation[i] - agility_variation[i - 1] for i in range(1, len(agility_variation))]
+    expertise_rate = [0] + [expertise_variation[i] - expertise_variation[i - 1] for i in range(1, len(expertise_variation))]
+    resistance_rate = [0] + [resistance_variation[i] - resistance_variation[i - 1] for i in range(1, len(resistance_variation))]
+    hp_rate = [0] + [hp_variation[i] - hp_variation[i - 1] for i in range(1, len(hp_variation))]
+
+    generations = list(range(0, len(results["strength_variation"]) + 1))
+
+    strength_trace = go.Scatter(x=generations, y=strength_rate, mode='markers', name='Strength Rate of Change')
+    agility_trace = go.Scatter(x=generations, y=agility_rate, mode='markers', name='Agility Rate of Change')
+    expertise_trace = go.Scatter(x=generations, y=expertise_rate, mode='markers', name='Expertise Rate of Change')
+    resistance_trace = go.Scatter(x=generations, y=resistance_rate, mode='markers',
+                                  name='Resistance Rate of Change')
+    hp_trace = go.Scatter(x=generations, y=hp_rate, mode='markers', name='HP Rate of Change')
+
+    layout = go.Layout(
+        title='Rate of Change of Genetic Algorithm Variations',
+        xaxis=dict(title='Generation'),
+        yaxis=dict(title='Rate of Change'),
+        xaxis_range=[0, len(generations) - 1]
+    )
+
+    # Create the figure and show it
+    fig = go.Figure(data=[strength_trace, agility_trace, expertise_trace, resistance_trace, hp_trace], layout=layout)
+    fig.show()
+
 
 def generate_best_performance_graph(results):
-
 
     generations = list(range(0, len(results["best_performance_list"]) + 1))
 
@@ -59,6 +92,7 @@ def generate_graphs(file_name=None):
 
     generate_best_performance_graph(results)
     generate_gene_variation_graph(results)
+    generate_variation_rate_of_change_graph(results)
 
 
 generate_graphs()
