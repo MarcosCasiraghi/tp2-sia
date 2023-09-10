819ef1fbf51f902466fbea11a9e324a487fbc9f8
Agustin Z <86991224+azakalik@users.noreply.github.com>
2023-09-10 20:07:45 -0300

    Completed structure cutoff method

--- cutoff_criteria/base_cutoff.py
+++ cutoff_criteria/base_cutoff.py
@@ -1,6 +1,7 @@
 from cutoff_criteria.acceptable_solution import acceptable_solution_cutoff
 from cutoff_criteria.content import content_cutoff
 from cutoff_criteria.generation_number import generation_cutoff
+from cutoff_criteria.structure import structure_cutoff
 
 
 def cutoff(populus, config, prev_gen_info):
@@ -12,6 +13,8 @@ def cutoff(populus, config, prev_gen_info):
         return content_cutoff(populus, config["cutoff_criteria"]["delta"], config["cutoff_criteria"]["max_gen_unchanged"], prev_gen_info)
     elif method == "acceptable_solution_cutoff":
         return acceptable_solution_cutoff(populus, config["cutoff_criteria"]["acceptable_fitness"])
+    elif method == "structure_cutoff":
+        return structure_cutoff(populus, config["cutoff_criteria"]["delta"], config["cutoff_criteria"]["max_gen_unchanged"], config["cutoff_criteria"]["percentage"], prev_gen_info)
     else:
         quit("Invalid cutoff method")
 
--- cutoff_criteria/structure.py
+++ cutoff_criteria/structure.py
@@ -1,45 +1,68 @@
 from classes.generation import Generation
-from functools import reduce
-from classes.fighters import Fighter
+from collections import deque
 
-#parte relevante => que % de poblacion no cambia para que criterio de corte se de
-# ej: Quiero que 80% no cambie en 
-# ej: Que no cambia quiere decir que la diferencia entre alelos no pasa mas que un delta
-# ej: No cambiar => Alelos iguales
-# ej: Altura no cambia
+def structure_cutoff(gen: Generation, delta, generation_quantity, percentage, prev_gen_info):
+    if 'structure_cutoff_info' not in prev_gen_info:
+        prev_gen_info['structure_cutoff_info'] = deque(maxlen=generation_quantity)
 
-def obtain_attribute_sum(gen, attribute):
-    sum = 0
-    for elem in gen:
-        sum += getattr(elem,attribute)
-    return sum / len(gen)
-
-def structure_cutoff( gen: Generation ,prev_gen_info, delta, delta_height):
-    attributes = ["height", "agility", "strength", "expertise", "resistance", "hp"]
-
-    # Inicializamos valores por ser la primera vez
-    if "num_unchanged_generations" not in prev_gen_info:
-        prev_gen_info["num_unchanged_generations"] = 0
-
-        # iteramos por los atributos
-        # sacamos le promedio de cada uno
-        for attribute in attributes:
-            prev_gen_info[f"avg_{attribute}"] = sum(getattr(fighter, attribute) for fighter in gen) / len(gen)
+    sorted_generation = sorted(gen, reverse=True)  # sort the current generation from best to worst in performance
+    generations_performance_info = prev_gen_info['structure_cutoff_info']
+    generations_performance_info.append({
+        'best': get_first_x_percent(sorted_generation, percentage),
+        'worst': get_last_x_percent(sorted_generation, percentage),
+        'mean': get_mean(sorted_generation)
+    })
+    if len(generations_performance_info) < generation_quantity:
         return False
 
-    #si es una generacion que no es la primera
-    avg_height = obtain_attribute_sum(gen, "height")
-    avg_agility = obtain_attribute_sum(gen,"agility")
-    avg_strength = obtain_attribute_sum(gen,"strength")
-    avg_expertise = obtain_attribute_sum(gen,"expertise")
-    avg_resistance = obtain_attribute_sum(gen,"resistance")
-    avg_hp = obtain_attribute_sum(gen,"hp")
-    # comparamos
-    if prev_gen_info["avg_height"] - avg_height < delta:
-        
-    
+    mean_performances = get_mean_performances(generations_performance_info)
+    for index in mean_performances:
+        for gen_info in generations_performance_info:
+            if abs(mean_performances[index] - gen_info[index]) / mean_performances[index] > delta:
+                return False
+    return True
 
 
+# get mean performance of the best X percent of population
+def get_first_x_percent(gen, percentage):
+    amount = round(len(gen) * percentage)
+    first_x_percent = gen[:amount]
+    sum = 0
+    for fighter in first_x_percent:
+        sum += fighter.calc_performance()
+    return sum / amount
 
 
+# get mean performance of the worst X percent of population
+def get_last_x_percent(gen, percentage):
+    amount = round(len(gen) * percentage)
+    last_x_percent = gen[-amount:]
+    sum = 0
+    for fighter in last_x_percent:
+        sum += fighter.calc_performance()
+    return sum / amount
 
+
+# get mean performance of all population
+def get_mean(gen):
+    sum = 0
+    for fighter in gen:
+        sum += fighter.calc_performance()
+    return sum / len(gen)
+
+
+def get_mean_performances(generations_performance_info):
+    mean_performances = {
+        "best": 0,
+        "worst": 0,
+        "mean": 0
+    }
+
+    for index in mean_performances:
+        for gen_performance in generations_performance_info:
+            mean_performances[index] += gen_performance[index]
+
+    for index in mean_performances:
+        mean_performances[index] /= len(generations_performance_info)
+
+    return mean_performances
